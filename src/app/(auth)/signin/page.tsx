dafd3a2c7b94499a20a3ad6b10f5557520b11b08
zahrachegini <zch6511@gmail.com>
2023-11-26 23:00:35 +0330

    :fire: First commit

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+export default async function SingIn() {
+  return (
+    <div className="text-5xl flex justify-center items-center w-full">
+      <h1>This is signin page</h1>
+    </div>
+  );
+}
---
+++ src/app/(auth)/verify/page.tsx
@@ -0,0 +1,8 @@
+export default async function Verify() {
+    return (
+      <div className="text-5xl flex justify-center items-center w-full">
+        <h1>This is Verify page</h1>
+      </div>
+    );
+  }
+  
