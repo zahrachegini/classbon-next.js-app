d5ccff36eec3fc9d4ccafd7f22cca4899e964952
zahrachegini <zch6511@gmail.com>
2023-11-29 14:29:03 +0330

    Customize font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,49 @@
 import "./globals.css";
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+const iranyekan = localFont({
+  src: [
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanWebThin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanWebLight.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanWebRegular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanWebBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanWebExtraBold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanWebBlack.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-iranyekan",
+});
 
 export default function RootLayout({
   children,
@@ -6,10 +51,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl">
+    <html
+      lang="en"
+      dir="rtl"
+      className={`${figtree.variable} ${iranyekan.variable}`}
+    >
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          header
+          هدر
         </header>
         <div className="flex-1 flex justify-center items-center">
           {children}
